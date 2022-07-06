9d894d191368cc576a98e858d4f9913fa980cef8
adityofr9 <adityofr9@gmail.com>
2022-07-06 10:13:13 +0700

    menggunakan literal dan menambahkan umur

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,4 +1,6 @@
 // Variable Nama
-let name= "Muhammad Adityo Fathur Rahim";
+let name = "Muhammad Adityo Fathur Rahim";
+let age = 24;
 
 console.info("hello world, ", name);
+console.log(`hellow world, Nama saya ${name}, Umur saya ${age}`);
