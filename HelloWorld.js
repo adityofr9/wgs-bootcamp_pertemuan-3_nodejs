20682045f9dae582e61b42be953b3f42d28ee029
adityofr9 <adityofr9@gmail.com>
2022-07-06 10:30:55 +0700

    console.info dihilangkan

--- HelloWorld.js
+++ HelloWorld.js
@@ -2,5 +2,5 @@
 let name = "Muhammad Adityo Fathur Rahim";
 let age = 24;
 
-console.info("hello world, ", name);
+// console.info("hello world, ", name);
 console.log(`hellow world, Nama saya ${name}, Umur saya ${age}`);
